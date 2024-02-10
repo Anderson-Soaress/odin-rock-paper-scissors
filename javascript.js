b4c7209a0a6849423d8fc362e86e146dd8cfe540
Anderson Soares <andersonsoares.jn@gmail.com>
2024-02-10 15:58:17 -0300

    feat: Add game to work with HTML and CSS Now is possible to "play" the game.

--- javascript.js
+++ javascript.js
@@ -1,33 +1,29 @@
+function game(){
+    let playerChoice;
+    let computerChoice;
+    let result;
+
+    const choiceBtns = document.querySelectorAll(".btn");
+    choiceBtns.forEach(button => button.addEventListener("click" , () =>{
+
+        playerChoice = button.value;
+        computerChoice = getComputerChoice();
+
+        result = whoWin(playerChoice, computerChoice);
+
+        scoreboard(result);
+    }))
+}
+
 function getComputerChoice(){
-    let randomChoice = Math.floor(Math.random() * 3);
-    if(randomChoice == 0){
-        computerSelection = "rock";
-    } else if(randomChoice == 1){
-        computerSelection = "paper";        
-    } else {
-        computerSelection = "scissors";
-    }
+    computerChoice = ["rock", "paper", "scissors"];
+    randomChoice = Math.floor(Math.random() * 3);
+    computerSelection = computerChoice[randomChoice];
 
-    return computerSelection;
+    return computerSelection
 }
 
-function getPlayerChoice(){
-    let playerSelection = prompt("Choose: Rock, paper and scissors!!");
-    let invalidChoice = true;
-
-    while(invalidChoice){
-        playerSelection = playerSelection.toLowerCase();
-        if(playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors"){   
-            invalidChoice = false;
-        } else {
-            alert("Can't understand your choice, make sure you're typing correctly.");
-            playerSelection = prompt("Try again: Rock, paper and scissors!!");
-        }
-    }
-    return playerSelection;
-}
-
-function playRound(playerSelection, computerSelection){
+function whoWin(playerSelection, computerSelection){
     if((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper")){
         alert(" You win! " + playerSelection + " beats " + computerSelection);
         return "player"
@@ -38,22 +34,6 @@ function playRound(playerSelection, computerSelection){
         alert("It's a draw!!")
         return "draw"
     }
-} 
-
-function playGame(){
-    
-    let playerPoints = 0;
-    let computerPoints = 0;
-    while(playerPoints != 3 && computerPoints != 3){
-        let whoWin = playRound(getPlayerChoice(), getComputerChoice());
-        if(whoWin == "player"){
-            playerPoints = playerPoints + 1;
-        } else if (whoWin == "computer"){
-            computerPoints = computerPoints + 1;
-        } else{}
-
-    alert("Scoreboard:\nPlayer " + playerPoints + " x Computer " + computerPoints)
-    }
-    
 }
 
+game();
