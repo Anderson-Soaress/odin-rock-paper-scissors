d4b1ec2cf29e254997acb36f9c239f0708bb9f1b
Anderson Soares <andersonsoares.jn@gmail.com>
2024-02-11 10:28:38 -0300

    Fix: Adjust the clear problem.
    Now the screen is clear automatically, without needing a button anymore

--- javascript.js
+++ javascript.js
@@ -1,32 +1,51 @@
-function game(){
-    const divScoreboard = document.createElement("div");
-    const computerChoiceParagraph = document.createElement("p");
-    const whoWinParagraph = document.createElement("p");
-    const scoreboardParagraph = document.createElement("p");
-    const section = document.querySelector(".section");
+const divScoreboard = document.createElement("div");
+const computerChoiceParagraph = document.createElement("p");
+const whoWinParagraph = document.createElement("p");
+const scoreboardParagraph = document.createElement("p");
+const section = document.querySelector(".section");
 
+let textComputerChoice;
+let textWhoWin;
+let textScoreboard;
+
+textWhoWin = document.createTextNode(``)
+textComputerChoice = document.createTextNode(``);
+textScoreboard = document.createTextNode(``);
+section.appendChild(divScoreboard);
+
+computerChoiceParagraph.appendChild(textComputerChoice);
+whoWinParagraph.appendChild(textWhoWin);
+scoreboardParagraph.appendChild(textScoreboard);
+
+divScoreboard.appendChild(computerChoiceParagraph);
+divScoreboard.appendChild(whoWinParagraph);
+divScoreboard.appendChild(scoreboardParagraph);
+
+function game(){
     let playerChoice;
     let computerChoice;
     let result;
     let playerPoints = 0;
     let computerPoints = 0;
-    let textComputerChoice;
-    let textWhoWin;
-    let textScoreboard;
-    let firstTime = true;
 
     const choiceBtns = document.querySelectorAll(".btn");
     choiceBtns.forEach(button => button.addEventListener("click" , () =>{
 
-        section.appendChild(divScoreboard);
+        computerChoiceParagraph.removeChild(textComputerChoice);
+        whoWinParagraph.removeChild(textWhoWin);
+        scoreboardParagraph.removeChild(textScoreboard);
+        divScoreboard.removeChild(computerChoiceParagraph);
+        divScoreboard.removeChild(whoWinParagraph);
+        divScoreboard.removeChild(scoreboardParagraph); 
+        section.removeChild(divScoreboard);
 
         playerChoice = button.value;
         computerChoice = getComputerChoice();
 
         result = whoWin(playerChoice, computerChoice);
-        
+
         textComputerChoice = document.createTextNode(`The computer choose: ${computerChoice}`);
-        
+
         if(result == "player"){
             textWhoWin = document.createTextNode(`You win, ${playerChoice} beats ${computerChoice}!!`)
             playerPoints = playerPoints + 1;
@@ -37,8 +56,9 @@ function game(){
             textWhoWin = document.createTextNode("No winners!! It's a draw.")
         }
         
-        textScoreboard = document.createTextNode(`You ${playerPoints} x ${computerPoints} Computer.`)
+        textScoreboard = document.createTextNode(`You ${playerPoints} x ${computerPoints} Computer.`);
 
+        section.appendChild(divScoreboard);
         computerChoiceParagraph.appendChild(textComputerChoice);
         whoWinParagraph.appendChild(textWhoWin);
         scoreboardParagraph.appendChild(textScoreboard);
@@ -46,25 +66,6 @@ function game(){
         divScoreboard.appendChild(computerChoiceParagraph);
         divScoreboard.appendChild(whoWinParagraph);
         divScoreboard.appendChild(scoreboardParagraph);
-
-        const btnRemove = document.createElement("button");
-        const textBtnRemove = document.createTextNode("Remove");
-        btnRemove.appendChild(textBtnRemove);
-        divScoreboard.appendChild(btnRemove)
-
-        btnRemove.addEventListener("click", () => {
-            divScoreboard.removeChild(btnRemove);
-            computerChoiceParagraph.removeChild(textComputerChoice);
-            whoWinParagraph.removeChild(textWhoWin);
-            scoreboardParagraph.removeChild(textScoreboard);
-            divScoreboard.removeChild(computerChoiceParagraph);
-            divScoreboard.removeChild(whoWinParagraph);
-            divScoreboard.removeChild(scoreboardParagraph);
-            
-            section.removeChild(divScoreboard);
-        })
-
-        console.log(textComputerChoice);
     }))
 }
 
